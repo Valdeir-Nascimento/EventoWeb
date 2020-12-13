0ea9f4ef9343c20c3a2feeb351c47892fc76f856
valdeir.nascimento <valdeircn11@gmail.com>
2020-12-13 18:43:19 -0300

    Import inicial

---
+++ src/main/java/com/eventoweb/eventoweb/EventowebApplication.java
@@ -0,0 +1,13 @@
+package com.eventoweb.eventoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventowebApplication.class, args);
+	}
+
+}
